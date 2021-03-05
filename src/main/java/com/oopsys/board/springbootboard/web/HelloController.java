9a8ae2d73b9f6eb435c32bc317454bcf902fbbfb
oopsys6 <operasystem6@naver.com>
2021-03-05 18:57:44 +0900

    test: controller 테스트

---
+++ src/main/java/com/oopsys/board/springbootboard/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.oopsys.board.springbootboard.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
