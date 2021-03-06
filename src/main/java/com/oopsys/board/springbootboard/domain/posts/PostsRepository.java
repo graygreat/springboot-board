0494de4a7e6ac63d24c6aaa33899d7eca6c68f16
oopsys6 <operasystem6@naver.com>
2021-03-06 18:42:14 +0900

    feat: posts domain 구성 및 repository 생성

---
+++ src/main/java/com/oopsys/board/springbootboard/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.oopsys.board.springbootboard.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
