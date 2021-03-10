217c6c369c5eb26e190fa33036826e14dcb8cd09
oopsys6 <operasystem6@naver.com>
2021-03-10 19:07:58 +0900

    feat: index controller

---
+++ src/main/java/com/oopsys/board/springbootboard/web/IndexController.java
@@ -0,0 +1,19 @@
+package com.oopsys.board.springbootboard.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}
