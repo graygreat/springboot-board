6978bf9b7aade4817b3d43ecb7c6dc0ea4d4c6ca
oopsys6 <operasystem6@naver.com>
2021-03-10 19:08:11 +0900

    test: 메인 페이지 로딩 확인

---
+++ src/test/java/com/oopsys/board/springbootboard/web/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package com.oopsys.board.springbootboard.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() throws Exception {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        Assertions.assertThat(body).contains("게시판을 만들어보자");
+    }
+}
